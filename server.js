44fbd78a0140c3847d24649c67116b8604e97958
kazuchan1889 <you@example.com>
2024-09-23 14:38:32 +0700

    server change

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const Cfile = require("./src/Companyfile/routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Untuk mengizinkan semua domain (bisa juga disesuaikan)
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static("public"));
